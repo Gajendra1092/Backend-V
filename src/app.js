23c542a6b4308a2788287703c747b897e769f9e4
Gajendra <gajendranh1092@gmail.com>
2025-03-14 20:11:08 +0530

    Lect- 13, made controller wrapped in asyncHandler and made user.routes files

--- src/app.js
+++ src/app.js
@@ -25,4 +25,15 @@ app.use(express.urlencoded(
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes import (segration)
+import userRouter from "./routes/user.routes.js";
+
+// routes declarations
+// app.use("/users", userRouter);  used as a middleware here. Ref. example file in backend folder.
+app.use("/api/v1/users", userRouter); // standard pratice to define api version.
+
+
+
+
+
 export default app; // export {app}; both are same thing.
