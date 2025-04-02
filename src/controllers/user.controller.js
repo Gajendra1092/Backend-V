c7e3185ce37b28990598b1c9a9bf41ecc1ad625d
Gajendra <gajendranh1092@gmail.com>
2025-04-02 23:53:12 +0530

    added new nested pipelines for watch history, added new routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -351,6 +351,44 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 
 });
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+      const user = await User.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            },
+            $lookup:{
+                from: "videos",
+                localField: "_id",
+                foreignField: "owner",
+                as: "watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as:"owner",
+                            pipeline:[{
+                                $project:{
+                                    username:1,
+                                    fullName:1,
+                                    avatar:1,
+                                }
+                            },{
+                                $addFields:{
+                                    owner:{
+                                        $first: "$owner"
+                                    }
+                                }
+                            }]
+                        }
+            }]
+        }}
+      ])
+      return res.status(200).json(new ApiResponse(200, user[0]?.watchHistory, "Watch history found!"));
+});
+
 export {
     registerUser,
     loginUser,
@@ -361,4 +399,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-}
+    getUserChannelProfile,
+    getWatchHistory,
+ };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { changeCurrentPassword, loginUser, logOutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, loginUser, logOutUser, refreshAccessToken, registerUser, updateAccountDetails, getUserChannelProfile, updateUserAvatar, updateUserCoverImage, getWatchHistory } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
@@ -18,5 +18,10 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refreshToken").post(refreshAccessToken);
 router.route("/changePassword").post(verifyJWT, changeCurrentPassword);
-
+router.route("/Current_user").post(verifyJWT, getCurrentUser);
+router.route("/update_account_detail").patch(updateAccountDetails); // patch to update only selected details not full account detail.
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/coverImage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage); // cover image is also avatar in the database. So, we can use the same function to update the cover image.
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watchHistory").patch(verifyJWT, getWatchHistory);
 export default router;
